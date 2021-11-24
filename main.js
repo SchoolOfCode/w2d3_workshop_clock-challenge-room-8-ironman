0240d6472722e39a12a9ec79878524e0f5558001
Marco Tsang <hkmarcoot@gmail.com>
2021-11-24 12:02:24 +0000

    Finish The first requirement

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
+//Initialize the variable to be numbers
+let count = 0;
+let degree = 0;
+
+function move6DegreePerSec () {
+
+//Add one for every seconds
+    count++;
+//multiple by 6
+    degree = count * 6;
+    //console.log(degree);
+
+//Rotate the hand
+    hand.style.transform = `rotate(${degree}deg)`;// example of how to rotate the hand
+    //console.log("Clock"); 
+}
+
+//Using setInterval in here to move the hand every seconds
+let intervalID = setInterval(move6DegreePerSec, 1000);
